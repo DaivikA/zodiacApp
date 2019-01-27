52cab034b8a3149e9475bb1e468bc9abfdd96f5d
DaivikA <dacharya797@gmail.com>
2019-01-27 15:32:36 -0800

    few changes made

--- client/JS/scripts.js
+++ client/JS/scripts.js
@@ -1,20 +1,63 @@
-function getAlerted(){
-
-	var person1 ={name:'Daivik', age:14, location:'Cary', graduated: true};
-	var person2 ={name:'Rajib', age:50, location:'Georgia', graduated: false};
-	var person3 ={name:'Dibya', age:16, location:'Millburn', graduated: true};
-
-	//uses every single person in array
-	var people = [person1,person2,person3];
-	for(var i = 0; i < people.length; i++){
-		if(people[i].graduated == true){
-			document.getElementById('demo').innerHTML += people[i].name + '' + 
-			" is graduated." + "<br>";
-		}else{
-			document.getElementById('demo').innerHTML += people[i].name + '' + 
-			" is not  graduated." + "<br>";
-		}
-
-	}
-
+var zodiacs = [
+{
+	name: "Aries",
+	month: "March 21 - April 20",
+	description: "Aires is ruled by Mars",
 }
+{
+	name: "Taurus",
+	month: "April 20 to May 20",
+	description: "Sensual Taruis is ruled by Venus"
+
+},
+{
+	name: "Gemini",
+	month:"May 21 - June 20",
+	description: "The Gemini mind"
+},
+{
+	name:"Cancer",
+	month:"June 21 to July 22",
+	description:"Sensitive Cance zodiac is ruled"
+},
+{
+	name:"Leo",
+	month:"July 23 to August 22",
+	description: "The sun",
+},
+{
+	name:"Virgo",
+	month:"August 23 to Septmeber 22",
+	description: "Detailed-oriented Virgo"
+},
+{
+	name:"Libra",
+	month:"September 23 to Octob 22",
+	description: "Gentle Libra is ruled by Venus"
+},
+{
+	name: "Scorpio",
+	month:"October 23 to November 22",
+	description: "Intense Scorpio is ruled by Pluto"
+},
+{
+	name: "Suagittarius",
+	month: "November 22 to December 21",
+	description: "ruled by Jupiter"
+},
+{
+	name:"Capricorn",
+	month: "December 22 to Janurary 19",
+	description:"Ruled by Saturn"
+},
+{
+	name:"Aquarius",
+	month: "Janurary 20 to Feburary 18",
+	description:"Ruled by Uranus"
+},
+{
+	name:"Pisces",
+	month: "Feburary 19 to March 20"
+	description:"Fish Swimming"
+}
+]
